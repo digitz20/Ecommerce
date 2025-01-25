cd57044e3ad0ba36e5f51995eafa6183f944cdd1
digitz20 <alaekekaebuka200@gmail.com>
2025-01-25 15:03:09 +1345

    i updated db config

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const port = 2014
 const express = require("express")
+const cors = require("cors")
 const router = require("./router/storeRouter")
 const productRouter = require("./router/productRouter")
 
 const app = express()
-
+app.use(cors)
 app.use(express.json())
 
 app.use(router)
